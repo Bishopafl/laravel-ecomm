a5dbdf358a3bb7ba3ac2966b380f8a0e0f77978a
Adam Lopez <adamf04@gmail.com>
2023-02-16 19:14:33 -0500

    Adding the backend template. configuring the template to my laravel build. it's coming out alright.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,8 +10,10 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
-                    {{ Auth::user()->name }}
-                    {{ Auth::user()->email }}
+                    </br></br>
+                    name: {{ Auth::user()->name }} </br>
+                    email: {{ Auth::user()->email }} </br>
+                    username: {{ Auth::user()->username }} </br>
                 </div>
             </div>
         </div>
