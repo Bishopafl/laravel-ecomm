ee6b6f150c76c7e352300cb06b69b4e52e7c38c8
Adam Lopez <adamf04@gmail.com>
2023-02-18 10:50:44 -0500

    Setup the login/logout functionality, will customize the login and register pages.

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="sl-logo"><a href=""><i class="icon ion-android-star-outline"></i> eCommShop</a></div>
+<div class="sl-logo"><a href=""><i class="icon ion-android-star-outline"></i> {{ env('APP_NAME') }}</a></div>
 <div class="sl-sideleft">
     <div class="input-group input-group-search">
         <input type="search" name="search" class="form-control" placeholder="Search">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::controller(DemoController::class)->group(function () {
+    Route::get('/about', 'About')->name('about.page')->middleware('check');
+    Route::get('/contact', 'Contact')->name('contact.page');
+});
+
+/**
+ * Admin | All Routes
+ */
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+    
 });
 
 require __DIR__.'/auth.php';
